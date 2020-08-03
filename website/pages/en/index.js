492422dfe4f19f94527e7f179343c95b6cef75b1
Nikita Nek Dudnik <nikdudnik@gmail.com>
2020-08-03 13:32:19 +0200

    feat: update layout

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -143,31 +143,31 @@ class Index extends React.Component {
           <Block.Container wrapped>
             <Block.TextBox wide>
               <BulletLine />
-              <Block.Subtitle>Backstage software Templates <span>(alpha)</span></Block.Subtitle>
-              <Block.Title small>Standards can set you free</Block.Title>
+              <Block.Subtitle>Backstage TechDocs <span>(Coming Soon)</span></Block.Subtitle>
+              <Block.Title small>Docs like code</Block.Title>
             </Block.TextBox>
             <Block.TextBox small>
               <BulletLine />
-              <Block.SmallTitle small>Like automated getting started guides</Block.SmallTitle>
-              <Block.Paragraph>Using templates, engineers can spin up a new microservice with your organization’s best practices built-in, right from the start</Block.Paragraph>
-            </Block.TextBox>
-
-            <Block.TextBox small>
-              <BulletLine />
-              <Block.SmallTitle small>Push-button deployment</Block.SmallTitle>
-              <Block.Paragraph>Click a button to create a Spring Boot project with your repo automatically configured on GitHub and your CI already running the first build</Block.Paragraph>
+              <Block.SmallTitle small>Free documentation</Block.SmallTitle>
+              <Block.Paragraph>Whenever you use a Backstage Software Template, your project automatically gets a TechDocs site, for free</Block.Paragraph>
             </Block.TextBox>
 
             <Block.TextBox small>
               <BulletLine />
-              <Block.SmallTitle small>Built to your standards</Block.SmallTitle>
-              <Block.Paragraph>Go instead of Java? CircleCI instead of Jenkins? Serverless instead of Kubernetes? GCP instead of AWS? Customize your recipes with your best practices baked-in</Block.Paragraph>
+              <Block.SmallTitle small>Easy to write</Block.SmallTitle>
+              <Block.Paragraph>With our docs-like-code approach, engineers write their documentation in Markdown files right alongside their code</Block.Paragraph>
             </Block.TextBox>
 
             <Block.TextBox small>
               <BulletLine />
-              <Block.SmallTitle small>Golden Paths pave the way</Block.SmallTitle>
-              <Block.Paragraph>When the right way is also the easiest way, engineers get up and running faster — and more safely</Block.Paragraph>
+              <Block.SmallTitle small>Easy to maintain</Block.SmallTitle>
+              <Block.Paragraph>Updating code? Update your documentation while you’re there — with docs and code in the same place, it becomes a natural part of your workstream</Block.Paragraph>
+            </Block.TextBox>
+
+            <Block.TextBox small>
+              <BulletLine />
+              <Block.SmallTitle small>Easy to find and use</Block.SmallTitle>
+              <Block.Paragraph>Since all your documentation is in Backstage, finding any TechDoc is just a search query away</Block.Paragraph>
             </Block.TextBox>
             <img src={`${baseUrl}img/cards.png`} />
           </Block.Container>
@@ -183,76 +183,46 @@ class Index extends React.Component {
           </ActionBlock.Link>
         </ActionBlock>
 
-        <Block className="stripe bg-black">
-          <Block.Container>
-            <Block.TextBox>
-              <Block.Title>As simple as writing a plugin.</Block.Title>
-              <Block.Paragraph>Backstage makes unifying all of your infrastructure tooling, services, and documentation as simple as writing a plugin. With all your developer tools
-in one place, your engineers will always know where to find the right tool for the job. And they’ll already know how to use it, too — because now all your tools use the same, easy-to-use UI.</Block.Paragraph>
+        <Block className="stripe-top bg-teal-bottom" wrapped>
+          <Block.Container wrapped>
+            <Block.TextBox wide>
+              <BulletLine />
+              <Block.Subtitle>Customize Backstage with plugins</Block.Subtitle>
+              <Block.Title small>An app store for your infrastructure</Block.Title>
             </Block.TextBox>
-            <Block.Graphics>
-              <Breakpoint
-                wide={<Block.Graphic x={-5} y={-5} width={132} src={`${baseUrl}img/plugin.svg`} />}
-                narrow={<Block.Graphic x={-23.5} y={-5} width={132} src={`${baseUrl}img/plugin-mobile.svg`} />}
-              />
-            </Block.Graphics>
-          </Block.Container>
-        </Block>
-
-        <Block className="stripe bg-grey-black">
-          <Block.Container reversed>
-            <Block.TextBox>
-              <Block.Title>Developers are people, too.</Block.Title>
-              <Block.Paragraph>Why shouldn’t developer tools provide a first-class user experience? After all, engineers are people, too. Backstage is elegantly designed to make a diverse ecosystem of plugins, components, and frameworks easier to access and easier to use. How? By doing what other developer portals forget to consider: the developer on the other end of the portal.</Block.Paragraph>
+            <Block.TextBox small>
+              <BulletLine />
+              <Block.SmallTitle small>Add functionality</Block.SmallTitle>
+              <Block.Paragraph>Want scalable website testing? Add the Lighthouse plugin. Wondering about recommended frameworks? Add the Tech Radar plugin. </Block.Paragraph>
             </Block.TextBox>
-            <Block.Graphics>
-              <Block.Graphic x={-5} y={-5} width={120} src={`${baseUrl}img/developers.svg`} />
-            </Block.Graphics>
-          </Block.Container>
-        </Block>
 
-        <ActionBlock className="stripe bg-teal">
-          <ActionBlock.Title>
-            Read why we built it
-          </ActionBlock.Title>
-          <ActionBlock.Link href={`${baseUrl}background`}>
-            Our Story
-          </ActionBlock.Link>
-        </ActionBlock>
-
-        <Block className="stripe bg-black">
-          <Block.Container>
-            <Block.TextBox>
-              <Block.Title>An open platform inside and out.</Block.Title>
-              <Block.Paragraph>As a fully extensible platform, Backstage enables infrastructure teams to integrate new ideas from wherever they come from — whether that’s the open source community at large or the people who understand your infrastructure pain points the best: your own engineers. This extensibility is one reason why Backstage wasn’t just adopted, but embraced by Spotify’s own engineers.</Block.Paragraph>
+            <Block.TextBox small>
+              <BulletLine />
+              <Block.SmallTitle small>BYO Plugins</Block.SmallTitle>
+              <Block.Paragraph>If you don’t see the plugin you need, it’s simple to build your own</Block.Paragraph>
             </Block.TextBox>
-            <Block.Graphics>
-              <Block.Graphic x={-20} y={-10} width={140} src={`${baseUrl}img/open-platform.svg`} />
-            </Block.Graphics>
-          </Block.Container>
-        </Block>
 
-        <Block className="stripe bg-grey-black">
-          <Block.Container reversed>
-            <Block.TextBox>
-              <Block.Title>Compliance, privacy, and security, oh my!</Block.Title>
-              <Block.Paragraph>Managing compliance across five services is one thing. Managing 5,000 is quite another. Backstage makes it possible to scale and makes all the hairy bits less hairy. Plus, happier lawyers, accountants, and security officers also make for happier developers.</Block.Paragraph>
+            <Block.TextBox small>
+              <BulletLine />
+              <Block.SmallTitle small>Integrate your own custom tooling</Block.SmallTitle>
+              <Block.Paragraph>Building internal plugins lets you tailor your version of Backstage to be a perfect fit for your infrastructure</Block.Paragraph>
             </Block.TextBox>
-            <Block.Graphics>
-              <Breakpoint
-                wide={<Block.Graphic x={-33} y={-15} width={150} src={`${baseUrl}img/compliance.svg`} />}
-                narrow={<Block.Graphic x={-25} y={-40} width={150} src={`${baseUrl}img/compliance.svg`} />}
-              />
-            </Block.Graphics>
+
+            <Block.TextBox small>
+              <BulletLine />
+              <Block.SmallTitle small>Share with the community</Block.SmallTitle>
+              <Block.Paragraph>Building open source plugins contributes to the entire Backstage ecosystem, which benefits everyone</Block.Paragraph>
+            </Block.TextBox>
+            <img src={`${baseUrl}img/cards.png`} />
           </Block.Container>
         </Block>
 
         <ActionBlock className="stripe-top bg-teal">
           <ActionBlock.Title>
-            Subscribe to our newsletter
+            Build a plugin
           </ActionBlock.Title>
           <ActionBlock.Link href='https://mailchi.mp/spotify/backstage-community'>
-            Subscribe
+            Contribute
           </ActionBlock.Link>
         </ActionBlock>
       </main>
